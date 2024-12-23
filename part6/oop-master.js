ad224770ca61e9f08d2b25b9ca2e2f30cd5bf08f
Muhammad Arslan <imarslan7061@gmail.com>
2024-12-23 23:53:57 +0500

    Revision

--- part6/oop-master.js
+++ part6/oop-master.js
@@ -8,155 +8,164 @@ let car = {
 }
 console.log(car.Starts())
 
+// let car = {
+//   make: "Toyota",
+//   model: "Camry",
+//   year: 2020,
+//   start: function () {
+//     return `${this.make} car got started in ${this.year}`;
+//   },
+// };
+// // console.log(car.start());
 
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
-}
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
 
-let john = new Person("John Doe", 20);
-// console.log(john.name);
+// let john = new Person("John Doe", 20);
+// // console.log(john.name);
 
-function Animal(type) {
-  this.type = type;
-}
+// function Animal(type) {
+//   this.type = type;
+// }
 
-Animal.prototype.speak = function () {
-  return `${this.type} makes a sound`;
-};
+// Animal.prototype.speak = function () {
+//   return `${this.type} makes a sound`;
+// };
 
-Array.prototype.hitesh = function () {
-  return `Custom method ${this}`;
-};
+// Array.prototype.hitesh = function () {
+//   return `Custom method ${this}`;
+// };
 
-let myArray = [1, 2, 3];
-// console.log(myArray.hitesh());
-let myNewArray = [1, 2, 3, 4, 5, 6];
-// console.log(myNewArray.hitesh());
+// let myArray = [1, 2, 3];
+// // console.log(myArray.hitesh());
+// let myNewArray = [1, 2, 3, 4, 5, 6];
+// // console.log(myNewArray.hitesh());
 
-class Vehicle {
-  constructor(make, model) {
-    this.make = make;
-    this.model = model;
-  }
+// class Vehicle {
+//   constructor(make, model) {
+//     this.make = make;
+//     this.model = model;
+//   }
 
-  start() {
-    return `${this.model} is a car from ${this.make}`;
-  }
-}
+//   start() {
+//     return `${this.model} is a car from ${this.make}`;
+//   }
+// }
 
-class Car extends Vehicle {
-  drive() {
-    return `${this.make} : This is an inheritance example`;
-  }
-}
+// class Car extends Vehicle {
+//   drive() {
+//     return `${this.make} : This is an inheritance example`;
+//   }
+// }
 
-let myCar = new Car("Toyota", "Corolla");
-// console.log(myCar.start());
-// console.log(myCar.drive());
+// let myCar = new Car("Toyota", "Corolla");
+// // console.log(myCar.start());
+// // console.log(myCar.drive());
 
-let vehOne = new Vehicle("Toyota", "Corolla");
-// console.log(vehOne.make);
+// let vehOne = new Vehicle("Toyota", "Corolla");
+// // console.log(vehOne.make);
 
-// Encapsulation
+// // Encapsulation
 
-class BankAccount {
-  #balance = 0;
+// class BankAccount {
+//   #balance = 0;
 
-  deposit(amount) {
-    this.#balance += amount;
-    return this.#balance;
-  }
+//   deposit(amount) {
+//     this.#balance += amount;
+//     return this.#balance;
+//   }
 
-  getBalance() {
-    return `$ ${this.#balance}`;
-  }
-}
+//   getBalance() {
+//     return `$ ${this.#balance}`;
+//   }
+// }
 
-let account = new BankAccount();
-// console.log(account.getBalance());
+// let account = new BankAccount();
+// // console.log(account.getBalance());
 
-// Abstraction
+// // Abstraction
 
-class CoffeMachine {
-  start() {
-    // call DB
-    // filter value
-    return `Starting the machine...`;
-  }
-  brewCoffee() {
-    // complex calculation
-    return `Brewing coffee`;
-  }
+// class CoffeMachine {
+//   start() {
+//     // call DB
+//     // filter value
+//     return `Starting the machine...`;
+//   }
+//   brewCoffee() {
+//     // complex calculation
+//     return `Brewing coffee`;
+//   }
 
-  pressStartButton() {
-    let msgone = this.start();
-    let msgTwo = this.brewCoffee();
-    return `${msgone} + ${msgTwo}`;
-  }
-}
+//   pressStartButton() {
+//     let msgone = this.start();
+//     let msgTwo = this.brewCoffee();
+//     return `${msgone} + ${msgTwo}`;
+//   }
+// }
 
-let myMachine = new CoffeMachine();
-// console.log(myMachine.start());
-// console.log(myMachine.brewCoffee());
-// console.log(myMachine.pressStartButton());
+// let myMachine = new CoffeMachine();
+// // console.log(myMachine.start());
+// // console.log(myMachine.brewCoffee());
+// // console.log(myMachine.pressStartButton());
 
-// Polymorphism
+// // Polymorphism
 
-class Bird {
-  fly() {
-    return `Flying....`;
-  }
-}
+// class Bird {
+//   fly() {
+//     return `Flying....`;
+//   }
+// }
 
-class Penguin extends Bird {
-  fly() {
-    return `Penguins can't fly`;
-  }
-}
+// class Penguin extends Bird {
+//   fly() {
+//     return `Penguins can't fly`;
+//   }
+// }
 
-let bird = new Bird();
-let penguin = new Penguin();
-// console.log(bird.fly());
-// console.log(penguin.fly());
+// let bird = new Bird();
+// let penguin = new Penguin();
+// // console.log(bird.fly());
+// // console.log(penguin.fly());
 
-// static method
+// // static method
 
-class Calculator {
-  static add(a, b) {
-    return a + b;
-  }
-}
+// class Calculator {
+//   static add(a, b) {
+//     return a + b;
+//   }
+// }
 
-// let miniCalc = new Calculator();
-// console.log(miniCalc.add(2, 3));
+// // let miniCalc = new Calculator();
+// // console.log(miniCalc.add(2, 3));
 
-// console.log(Calculator.add(2, 3));
+// // console.log(Calculator.add(2, 3));
 
-// Getters and setters
+// // Getters and setters
 
-class Employee {
-  #salary;
-  constructor(name, salary) {
-    if (salary < 0) {
-      throw new Error("Salary cannot be in negative");
-    }
-    this.name = name;
-    this.#salary = salary;
-  }
+// class Employee {
+//   #salary;
+//   constructor(name, salary) {
+//     if (salary < 0) {
+//       throw new Error("Salary cannot be in negative");
+//     }
+//     this.name = name;
+//     this.#salary = salary;
+//   }
 
-  get salary() {
-    return `You are not allowed to see salary`;
-  }
+//   get salary() {
+//     return `You are not allowed to see salary`;
+//   }
 
-  set salary(value) {
-    if (value < 0) {
-      console.error("Invalid Salary");
-    } else {
-      this._salary = value;
-    }
-  }
-}
-let emp = new Employee("Alice", -50000);
+//   set salary(value) {
+//     if (value < 0) {
+//       console.error("Invalid Salary");
+//     } else {
+//       this._salary = value;
+//     }
+//   }
+// }
+// let emp = new Employee("Alice", -50000);
 // console.log(emp._salary);
 // emp.salary = 60000;
